97249beb44d82ffd9f5334080b65ca73794a413f
cargodog <cargodog@protonmail.com>
2019-11-05 01:40:38 -0500

    Remove verbosity guards from CLI interpreter logic

--- microcli.c
+++ microcli.c
@@ -32,10 +32,11 @@ static int lookup_command(MicroCLI_t * ctx)
 static inline void prompt_for_input(MicroCLI_t * ctx)
 {
     assert(ctx);
+    assert(ctx->cfg.io.printf);
 
     // Only display prompt once per command
     if(ctx->prompted == false) {
-        microcli_log(ctx, ctx->cfg.promptText);
+        ctx->cfg.io.printf(ctx->cfg.promptText);
         ctx->prompted = true;
     }
 }
@@ -43,54 +44,52 @@ static inline void prompt_for_input(MicroCLI_t * ctx)
 // Fill input buffer from IO stream. Return true when buffer is ready to process.
 static void read_from_io(MicroCLI_t * ctx)
 {
-    #if MAX_ALLOWED_VERBOSITY > VERBOSITY_LEVEL_DISABLED
-        assert(ctx);
-        assert(ctx->cfg.io.printf);
-        assert(ctx->cfg.io.getchar);
+    assert(ctx);
+    assert(ctx->cfg.io.printf);
+    assert(ctx->cfg.io.getchar);
 
-        // Convenience mapping
-        char * buffer = ctx->input.buffer;
+    // Convenience mapping
+    char * buffer = ctx->input.buffer;
 
-        // Loop through all available data
-        while(ctx->input.len < sizeof(ctx->input.buffer)) {
-            // Get next character
-            buffer[ctx->input.len] = ctx->cfg.io.getchar();
+    // Loop through all available data
+    while(ctx->input.len < sizeof(ctx->input.buffer)) {
+        // Get next character
+        buffer[ctx->input.len] = ctx->cfg.io.getchar();
 
-            // Handle escape characters
-            if( buffer[ctx->input.len] == 0 ||     // No more data
-                buffer[ctx->input.len] == '\n' ||  // New line
-                buffer[ctx->input.len] == '\r' )   // Carriage return
-                break;
+        // Handle escape characters
+        if( buffer[ctx->input.len] == 0 ||     // No more data
+            buffer[ctx->input.len] == '\n' ||  // New line
+            buffer[ctx->input.len] == '\r' )   // Carriage return
+            break;
 
-            // Handle backspace
-            if(buffer[ctx->input.len] == '\b') {
-                if(ctx->input.len > 0) {
-                    // Overwrite prev char from screen
-                    ctx->cfg.io.printf("\b \b");
-                    ctx->input.len--;
-                }
-
-                // Erase from buffer
-                buffer[ctx->input.len] = 0;
-            } else {
-                // Echo back
-                ctx->cfg.io.printf("%c", buffer[ctx->input.len]);
-                ctx->input.len++;
+        // Handle backspace
+        if(buffer[ctx->input.len] == '\b') {
+            if(ctx->input.len > 0) {
+                // Overwrite prev char from screen
+                ctx->cfg.io.printf("\b \b");
+                ctx->input.len--;
             }
-        }
 
-        // No more data to process. Wait for more data.
-        if(buffer[ctx->input.len] == 0) {
-            ctx->input.ready = false;
-        } else {
-            // Replace escape character with null terminator
+            // Erase from buffer
             buffer[ctx->input.len] = 0;
-            ctx->cfg.io.printf("\n\r");
-
-            // Command entry is complete. Input buffer is ready to be processed
-            ctx->input.ready = true;
+        } else {
+            // Echo back
+            ctx->cfg.io.printf("%c", buffer[ctx->input.len]);
+            ctx->input.len++;
         }
-    #endif
+    }
+
+    // No more data to process. Wait for more data.
+    if(buffer[ctx->input.len] == 0) {
+        ctx->input.ready = false;
+    } else {
+        // Replace escape character with null terminator
+        buffer[ctx->input.len] = 0;
+        ctx->cfg.io.printf("\n\r");
+
+        // Command entry is complete. Input buffer is ready to be processed
+        ctx->input.ready = true;
+    }
 }
 
 void microcli_init(MicroCLI_t * ctx, const MicroCLICfg_t * cfg)
@@ -119,46 +118,44 @@ void microcli_set_verbosity(MicroCLI_t * ctx, int verbosity)
 
 void microcli_interpreter_tick(MicroCLI_t * ctx)
 {
-    #if MAX_ALLOWED_VERBOSITY > VERBOSITY_LEVEL_DISABLED
-        assert(ctx);
-        assert(ctx->cfg.io.printf);
-        assert(ctx->cfg.io.getchar);
-        assert(ctx->cfg.promptText);
+    assert(ctx);
+    assert(ctx->cfg.io.printf);
+    assert(ctx->cfg.io.getchar);
+    assert(ctx->cfg.promptText);
 
-        // If input buffer does not contain a command, read more data from IO
-        if(!ctx->input.ready) {
-            // Prompt user for input
-            prompt_for_input(ctx);
+    // If input buffer does not contain a command, read more data from IO
+    if(!ctx->input.ready) {
+        // Prompt user for input
+        prompt_for_input(ctx);
 
-            // Read user input from IO
-            read_from_io(ctx);
-        }
-        
-        // If the input buffer contains a command, process it.
-        if(ctx->input.ready) {
-            // Lookup and run command (if found)
-            int cmdIdx = lookup_command(ctx);
-            if(cmdIdx >= 0 && cmdIdx < ctx->cfg.cmdCount) {
-                ctx->cfg.cmdTable[cmdIdx].cmd();
-                ctx->cfg.io.printf("\n\r");
-            }
-
-            // Reset prompt
-            ctx->prompted = false;
+        // Read user input from IO
+        read_from_io(ctx);
+    }
+    
+    // If the input buffer contains a command, process it.
+    if(ctx->input.ready) {
+        // Lookup and run command (if found)
+        int cmdIdx = lookup_command(ctx);
+        if(cmdIdx >= 0 && cmdIdx < ctx->cfg.cmdCount) {
+            ctx->cfg.cmdTable[cmdIdx].cmd();
+            ctx->cfg.io.printf("\n\r");
         }
 
-        // Reset the prompt, after processing a command
-        if(ctx->input.ready) {
-            memset(&ctx->input, 0, sizeof(ctx->input));
-            ctx->prompted = false;
-        }
-        
-    #endif
+        // Reset prompt
+        ctx->prompted = false;
+    }
+
+    // Reset the prompt, after processing a command
+    if(ctx->input.ready) {
+        memset(&ctx->input, 0, sizeof(ctx->input));
+        ctx->prompted = false;
+    }
 }
 
 int microcli_interpret_string(MicroCLI_t * ctx, const char * str, bool print)
 {
     assert(ctx);
+    assert(ctx->cfg.io.printf);
     assert(str);
 
     if(strlen(str) > sizeof(ctx->input.buffer))
@@ -178,7 +175,7 @@ int microcli_interpret_string(MicroCLI_t * ctx, const char * str, bool print)
         prompt_for_input(ctx);
         
         // Print the command to the console
-        microcli_log(ctx, "%s\n\r", str);
+        ctx->cfg.io.printf("%s\n\r", str);
     }
 
     // Tick the interpreter to process the command
@@ -189,35 +186,27 @@ int microcli_interpret_string(MicroCLI_t * ctx, const char * str, bool print)
 
 int microcli_banner(MicroCLI_t * ctx)
 {
-    #if MAX_ALLOWED_VERBOSITY > VERBOSITY_LEVEL_DISABLED
-        assert(ctx);
-        assert(ctx->cfg.bannerText);
-        assert(ctx->cfg.io.printf);
-        
-        return ctx->cfg.io.printf(ctx->cfg.bannerText);
-    #else
-        return 0;
-    #endif
+    assert(ctx);
+    assert(ctx->cfg.bannerText);
+    assert(ctx->cfg.io.printf);
+    
+    return ctx->cfg.io.printf(ctx->cfg.bannerText);
 }
 
 int microcli_help(MicroCLI_t * ctx)
 {
-    #if MAX_ALLOWED_VERBOSITY > VERBOSITY_LEVEL_DISABLED
-        assert(ctx);
-        assert(ctx->cfg.io.printf);
-        assert(ctx->cfg.cmdTable);
-        assert(ctx->cfg.cmdCount >= 0);
+    assert(ctx);
+    assert(ctx->cfg.io.printf);
+    assert(ctx->cfg.cmdTable);
+    assert(ctx->cfg.cmdCount >= 0);
 
-        int printLen = 0;
-        for(int i = 0; i < ctx->cfg.cmdCount; i++) {
-            int cmdLen = ctx->cfg.io.printf("%s", ctx->cfg.cmdTable[i].name);
-            insert_spaces(ctx, MAX_CLI_CMD_LEN - cmdLen);
-            printLen += MAX_CLI_CMD_LEN;
-            printLen += ctx->cfg.io.printf("%s\n\r", ctx->cfg.cmdTable[i].help);
-        }
-        
-        return printLen;
-    #else
-        return 0;
-    #endif
+    int printLen = 0;
+    for(int i = 0; i < ctx->cfg.cmdCount; i++) {
+        int cmdLen = ctx->cfg.io.printf("%s", ctx->cfg.cmdTable[i].name);
+        insert_spaces(ctx, MAX_CLI_CMD_LEN - cmdLen);
+        printLen += MAX_CLI_CMD_LEN;
+        printLen += ctx->cfg.io.printf("%s\n\r", ctx->cfg.cmdTable[i].help);
+    }
+    
+    return printLen;
 }
